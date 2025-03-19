1afa5440f6dcaf1eadcc978948404aef2e160d7a
sehmusekin <sehmusekin17@gmail.com>
2025-03-19 21:03:40 +0300

    Ocak4.HaftaJavaOdev

--- Ocak4.HaftaJavaOdev/src/main/java/org/example/forDongusu.java
+++ Ocak4.HaftaJavaOdev/src/main/java/org/example/forDongusu.java
@@ -1,2 +1,9 @@
-package org.example;public class forDongusu {
+package org.example;
+
+public class forDongusu {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
 }
--- Ocak4.HaftaJavaOdev/src/main/java/org/example/sayılarınToplamı.java
+++ Ocak4.HaftaJavaOdev/src/main/java/org/example/sayılarınToplamı.java
@@ -1,2 +1,19 @@
-package org.example;public class sayılarınToplamı {
+package org.example;
+
+import java.util.Scanner;
+
+public class sayılarınToplamı {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı girin: ");
+        long sayi = scanner.nextInt();
+        long toplam = 0;
+
+        for (long i = 1; i <= sayi; i++) {
+            toplam += i;
+        }
+
+        System.out.println("1'den " + sayi + "'e kadar olan sayıların toplamı: " + toplam);
+        // kulanıcının  int değerinden daha buyuk sayı gırdığınde hata vermemesı için long kulandım
+    }
 }
